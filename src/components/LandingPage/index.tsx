60b9eca5cfcf3ea8736f1b133767868606e41398
z0bla <rn.blazana@gmail.com>
2023-05-08 20:43:12 +0200

    Adding some style for footer

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -5,15 +5,17 @@ import styles from "./LandingPage.module.css";
 function LandingPage(): JSX.Element {
   return (
     <main className={styles.page}>
-      <h1>Lorem ipsum dolor sit.</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque illo
-        laudantium asperiores labore molestiae dolor architecto at deleniti
-        quibusdam fuga velit odio dolorem, vel fugiat incidunt magni
-        voluptatibus tenetur alias, perferendis nihil. Facere tenetur ut ab
-        dolore odio animi atque cumque minus! Nobis sed, nemo nam culpa neque
-        assumenda tempora.
-      </p>
+      <div className={styles.article}>
+        <h1>Lorem ipsum dolor sit.</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque illo
+          laudantium asperiores labore molestiae dolor architecto at deleniti
+          quibusdam fuga velit odio dolorem, vel fugiat incidunt magni
+          voluptatibus tenetur alias, perferendis nihil. Facere tenetur ut ab
+          dolore odio animi atque cumque minus! Nobis sed, nemo nam culpa neque
+          assumenda tempora.
+        </p>
+      </div>
     </main>
   );
 }
