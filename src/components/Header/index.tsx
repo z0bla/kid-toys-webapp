b3f3e617cb5ce1b508723a3819ae3ebb305ca8f6
z0bla <rn.blazana@gmail.com>
2023-05-08 19:54:16 +0200

    Adding some style for header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,40 @@
 import React from "react";
 
 import styles from "./Header.module.css";
+import logo from "./logoForHeader.png";
 
 function Header(): JSX.Element {
   return (
-    <div className={styles.header}>
-      <h1>Kid Toyz</h1>
-      <nav>
-        <ul className={styles.list}>
+    <div className={styles.headerContainer}>
+      <div className={styles.header}>
+        <div className={styles.logoAndName}>
+          <img className={styles.logoImg} src={logo} alt="img" />
+          <h1 className={styles.nameOfPage}>Kid Toys</h1>
+        </div>
+        <nav className={styles.navBarHeader}>
+          <ul className={styles.list}>
+            <li>
+              <a href="#">Register</a>
+            </li>
+            <li>
+              <a href="#">Login</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <nav className={styles.navBar}>
+        <ul>
           <li>
-            <a href="#">Register</a>
+            <a href="#">Home</a>
           </li>
           <li>
-            <a href="#">Login</a>
+            <a href="#">Catalog</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
           </li>
         </ul>
       </nav>
