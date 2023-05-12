ec312f051f27ece8791138a77332671fc764753a
z0bla <rn.blazana@gmail.com>
2023-05-12 16:58:06 +0200

    Add some style on land page

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,20 +1,26 @@
 import React from "react";
 
 import styles from "./LandingPage.module.css";
+import imageToy1 from "./imageToy1.png";
 
 function LandingPage(): JSX.Element {
   return (
     <main className={styles.body}>
       <div className={styles.article}>
-        <h1>Lorem ipsum dolor sit.</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque illo
-          laudantium asperiores labore molestiae dolor architecto at deleniti
-          quibusdam fuga velit odio dolorem, vel fugiat incidunt magni
-          voluptatibus tenetur alias, perferendis nihil. Facere tenetur ut ab
-          dolore odio animi atque cumque minus! Nobis sed, nemo nam culpa neque
-          assumenda tempora.
-        </p>
+        <div className={styles.leftSide}>
+          <img src={imageToy1} className={styles.imageOfToy} />
+        </div>
+        <div className={styles.rightSide}>
+          <h1>Lorem ipsum dolor sit.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque illo
+            laudantium asperiores labore molestiae dolor architecto at deleniti
+            quibusdam fuga velit odio dolorem, vel fugiat incidunt magni
+            voluptatibus tenetur alias, perferendis nihil. Facere tenetur ut ab
+            dolore odio animi atque cumque minus! Nobis sed, nemo nam culpa
+            neque assumenda tempora.
+          </p>
+        </div>
       </div>
     </main>
   );
