5a9e90aef1a33209a2842c1c448e13fe83a1c195
Goran Stevanovic <stevanovicgoran@proton.me>
2023-08-09 11:56:02 +0200

    Change footer content

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,42 @@
 import React from "react";
 
+import Logo from "../Logo";
 import styles from "./Footer.module.css";
 
 function Footer(): JSX.Element {
   const year = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
-      <small>owned by SyntaxError company limited © {year}</small>
+      <Logo />
+      <p>
+        Spark laughter, learning, and endless fun with our enchanting range of
+        kid&apos;s toys that inspire imagination and bring joy to every moment
+      </p>
+      <div>
+        <h4>Company</h4>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
+            <li>
+              <a href="#">Shipping</a>
+            </li>
+            <li>
+              <a href="#">Returns</a>
+            </li>
+            <li>
+              <a href="#">Terms and Conditions</a>
+            </li>
+            <li>
+              <a href="#">Privacy Policy</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </footer>
   );
 }
